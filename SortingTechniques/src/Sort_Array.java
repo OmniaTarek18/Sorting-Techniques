e155f8d988f57711f1e607591246c337740f3f4a
RanaMohamed6720 <121067602+RanaMohamed6720@users.noreply.github.com>
2024-03-14 14:10:32 +0200

    creating sort_array class

---
+++ SortingTechniques/src/Sort_Array.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Sort_Array {
+	
+    List<Integer> array;
+
+    //constructor to initialize the array from a file
+    public Sort_Array() {
+        
+    }
+
+    // Simple Sort O(n2)
+    public void simpleSort(boolean intermediate) {
+    	
+
+    }
+
+    // Efficient Sort (Merge Sort) O(n lg n)
+    public void efficientSort(boolean intermediate) {
+
+    }
+
+
+    // Non-Comparison Sort O(n) 
+    public void nonComparisonSort(boolean intermediate) {
+
+    }
+    
+}
