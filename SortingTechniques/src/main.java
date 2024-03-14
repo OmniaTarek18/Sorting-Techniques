e6bb5e6823996849d7f58b56b6a6a83718dfef03
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-03-14 14:26:07 +0200

    add main file

---
+++ SortingTechniques/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        //Command Line Interface 
+        // take the file path from user 
+        // make object of Sort_Array class
+        // print menu to the user
+        // call proper functions 
+    }
+}
